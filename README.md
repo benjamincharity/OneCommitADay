cfbb63eeeb794ec1d938b4185d42befc0656733f
Benjamin Charity <ben@benjamincharity.com>
2013-07-21 16:07:00 -0400

    removed directives and have controllers doing all the work

---
+++ README.md
@@ -0,0 +1,8 @@
+## One Commit A Day
+
+- - -
+
+gem install jekyll
+gem install redcarpet
+gem install compass
+gem install breakpoint
--- js/controllers.js
+++ js/controllers.js
@@ -9,28 +9,21 @@ var controllers = {};
 //
 // Controller 1
 controllers.SimpleController = function($scope, $http) {
-  var user = 'benjamincharity';
 
-  $http({method: 'GET', url: 'https://api.github.com/users/' + user}).
-    success(function(data, status, headers, config) {
-      // this callback will be called asynchronously
-      // when the response is available
-      //console.log(data, status);
-      $scope.user = data;
-
-    }).
-    error(function(data, status, headers, config) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-      console.log(data, status);
-    });
-
-  //$scope.changeUser = function(  ) {
-    ////user = newUser.name;
-    //console.log(user);
-    //console.log(newUser.name);
-  //};
-
+  $scope.loadData = function() {
+    $http({method: 'GET', url: 'https://api.github.com/users/' + $scope.typedName}).
+      success(function(data, status, headers, config) {
+        // this callback will be called asynchronously
+        // when the response is available
+        //console.log(data, status);
+        $scope.user = data;
+      }).
+      error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        console.log(data, status);
+      });
+  }
 };
 
 //
